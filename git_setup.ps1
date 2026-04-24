6bc60685c2cafa5340d4a35a95e89cb0423f0d75
jebrandao <jebrandao@firjan.com.br>
2026-04-24 10:53:08 -0300

    Projeto taskflow

--- git_setup.ps1
+++
@@ -1,7 +0,0 @@
-cd 'f:\Curso FullStack\taskflow'
-git add .
-git commit -m "Implement complete task management app with Angular 21"
-git branch -M main
-Write-Host "Please run the following commands manually:"
-Write-Host "git remote add origin https://github.com/YOUR_USERNAME/YOUR_REPO_NAME.git"
-Write-Host "git push -u origin main"
